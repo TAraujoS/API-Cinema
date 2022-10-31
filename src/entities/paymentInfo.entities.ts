af9f032651eea5126733981e0020c9bb3a6b5cdd
Ricardo Martins <102545943+ORMartins02@users.noreply.github.com>
2022-10-31 17:30:45 -0300

    fix: Entity name correction

--- src/entities/paymentInfo.entities.ts
+++ src/entities/paymentInfo.entities.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity("payment_infos")
+@Entity("paymentInfo")
 export class PaymentInfo {
   @PrimaryGeneratedColumn("uuid")
   readonly id: string;
