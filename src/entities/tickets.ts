a10409a91d9a6c25370e90fd8733389e4a3f868d
amonfanticelli <amon_fanticelli@hotmail.com>
2022-11-01 09:24:06 -0300

    feat: entity fix

--- src/entities/tickets.ts
+++ src/entities/tickets.ts
@@ -1,5 +1,4 @@
 import { Entity, Column, PrimaryColumn } from "typeorm";
-
 import { v4 as uuid } from "uuid";
 
 @Entity("tickets")
@@ -16,8 +15,8 @@ export class Ticket {
   @Column()
   chair: string;
 
-  @Column()
-  session_id: string;
+  //   @OneToOne(() => Session)
+  //   session: Session;
 
   constructor() {
     if (!this.id) {
