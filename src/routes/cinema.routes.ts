938f8110e8f093ff48263681a8f1f4849929853c
Larissa Sato <larissasato_@outlook.com>
2022-11-02 18:27:20 -0300

    created: middlewares idUser, isEmployeeAdm, update and added at routes

--- src/routes/cinema.routes.ts
+++ src/routes/cinema.routes.ts
@@ -1,10 +1,24 @@
 import { Router } from "express";
 import createCinemaController from "../controllers/cinema/createCinema.controller";
 import updateCinemaController from "../controllers/cinema/updateCinema.controller";
+import ensureAuthMiddleware from "../middlewares/ensureAuth.middleware";
+import ensureIsAdmMiddleware from "../middlewares/ensureIsAdm.middleware";
+import ensureUpdateMiddleware from "../middlewares/ensureUpdate.middleware";
 
 const cinemaRoutes = Router();
 
-cinemaRoutes.post("", createCinemaController);
-cinemaRoutes.patch("/:id", updateCinemaController);
+cinemaRoutes.post(
+  "",
+  ensureAuthMiddleware,
+  ensureIsAdmMiddleware,
+  createCinemaController
+);
+cinemaRoutes.patch(
+  "/:id",
+  ensureAuthMiddleware,
+  ensureIsAdmMiddleware,
+  ensureUpdateMiddleware,
+  updateCinemaController
+);
 
 export default cinemaRoutes;
--- src/routes/movies.routes.ts
+++ src/routes/movies.routes.ts
@@ -5,13 +5,31 @@ import { createMovieController } from "../controllers/movies/createMovies.contro
 import { listMoviesController } from "../controllers/movies/listMovies.controller";
 import { listMoviesByIdController } from "../controllers/movies/listMoviesById.controller";
 import { updateMovieController } from "../controllers/movies/updateMovie.controller";
+import ensureAuthMiddleware from "../middlewares/ensureAuth.middleware";
+import ensureIsAdmMiddleware from "../middlewares/ensureIsEmployeeAdm.middleware";
+import ensureIsEmployeeMiddleware from "../middlewares/ensureIsEmployeeAdm.middleware";
 
 const movieRoutes = Router();
 
-movieRoutes.post("", createMovieController);
-movieRoutes.get("", listMoviesController);
-movieRoutes.get("/:id", listMoviesByIdController);
-movieRoutes.patch("/:id", updateMovieController);
-movieRoutes.delete("/:id", deleteMovieController);
+movieRoutes.post(
+  "",
+  ensureAuthMiddleware,
+  ensureIsAdmMiddleware,
+  createMovieController
+);
+movieRoutes.get("", ensureAuthMiddleware, listMoviesController);
+movieRoutes.get("/:id", ensureAuthMiddleware, listMoviesByIdController);
+movieRoutes.patch(
+  "/:id",
+  ensureAuthMiddleware,
+  ensureIsEmployeeMiddleware,
+  updateMovieController
+);
+movieRoutes.delete(
+  "/:id",
+  ensureAuthMiddleware,
+  ensureIsAdmMiddleware,
+  deleteMovieController
+);
 
 export default movieRoutes;
