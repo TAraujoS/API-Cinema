bf33c4a92c02798a5d9f1ca9510b58de4ef6cd6d
amonfanticelli <amon_fanticelli@hotmail.com>
2022-11-04 14:16:18 -0300

    server.ts modified

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import express from "express";
 import "express-async-errors";
 import cinemaRoutes from "./routes/cinema.routes";
--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,12 @@ import app from "./app";
 import { AppDataSource } from "./data-source";
 import "dotenv/config";
 
-const PORT = process.env.PORT || 3000;
-
 (async () => {
   await AppDataSource.initialize().catch((err) => {
     console.error("Error during Data Source initialization", err);
   });
 
-  app.listen(PORT, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor executando");
   });
 })();
