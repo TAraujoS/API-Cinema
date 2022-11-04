64e4ba536e405a0762283a2b70aabdf056227dd6
amonfanticelli <amon_fanticelli@hotmail.com>
2022-11-04 16:57:24 -0300

    heroku tests

--- src/data-source.ts
+++ src/data-source.ts
@@ -34,22 +34,3 @@ export const AppDataSource = new DataSource(
             : ["src/migrations/*.ts"],
       }
 );
-
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  url: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
-  synchronize: false,
-  logging: true,
-  entities:
-    process.env.NODE_ENV === "production"
-      ? ["dist/entities/*.js"]
-      : ["src/entities/*.ts"],
-  migrations:
-    process.env.NODE_ENV === "production"
-      ? ["dist/migrations/*.js"]
-      : ["src/migrations/*.ts"],
-});
