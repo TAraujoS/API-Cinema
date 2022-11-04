4c5da3c77b3880fbf09da81f4b171966337e86d9
Larissa Sato <larissasato_@outlook.com>
2022-11-04 15:12:11 -0300

    refactor server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,12 @@ import app from "./app";
 import { AppDataSource } from "./data-source";
 import "dotenv/config";
 
-const PORT = process.env.PORT;
-
-// app.listen(PORT, async () => {
-//   AppDataSource.initialize()
-//     .then(() => console.log("Database initialized"))
-//     .catch((err) => console.error(err));
-
-//   console.log(`Server is running on localhost:${PORT}`);
-// });
-
 (async () => {
   await AppDataSource.initialize().catch((err) => {
     console.error("Error during Data Source initialization", err);
   });
 
-  app.listen(PORT, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor executando");
   });
 })();
