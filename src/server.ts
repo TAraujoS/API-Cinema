e97b38a11f372e1e3565bddca099c74b392b176d
amonfanticelli <amon_fanticelli@hotmail.com>
2022-11-04 13:49:41 -0300

    heroku and tests add

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,7 @@ import app from "./app";
 import { AppDataSource } from "./data-source";
 import "dotenv/config";
 
-const PORT = process.env.PORT;
-
-// app.listen(PORT, async () => {
-//   AppDataSource.initialize()
-//     .then(() => console.log("Database initialized"))
-//     .catch((err) => console.error(err));
-
-//   console.log(`Server is running on localhost:${PORT}`);
-// });
+const PORT = process.env.PORT || 3000;
 
 (async () => {
   await AppDataSource.initialize().catch((err) => {
