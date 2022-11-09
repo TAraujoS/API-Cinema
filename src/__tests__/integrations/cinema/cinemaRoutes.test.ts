326b4f02c5c3d60e2aa03341b032668a0af62478
Ricardo Martins <102545943+ORMartins02@users.noreply.github.com>
2022-11-09 11:31:03 -0300

    fix: tests errors

--- src/__tests__/integrations/cinema/cinemaRoutes.test.ts
+++ src/__tests__/integrations/cinema/cinemaRoutes.test.ts
@@ -1,108 +1,108 @@
-// import request from "supertest";
-// import { DataSource } from "typeorm";
-// import app from "../../../app";
-// import { AppDataSource } from "../../../data-source";
-// import {
-//   mockedAdmin,
-//   mockedAdminLogin,
-//   mockedCinema,
-//   mockedCinemaExistent,
-//   mockedCinemaUpdate,
-//   mockedEmployeeLogin,
-//   mockedUser,
-//   mockedUserLogin,
-// } from "../../mocks";
+import request from "supertest";
+import { DataSource } from "typeorm";
+import app from "../../../app";
+import { AppDataSource } from "../../../data-source";
+import {
+  mockedAdmin,
+  mockedAdminLogin,
+  mockedCinema,
+  mockedCinemaExistent,
+  mockedCinemaUpdate,
+  mockedEmployeeLogin,
+  mockedUser,
+  mockedUserLogin,
+} from "../../mocks";
 
-// describe("/cinema", () => {
-//   let connection: DataSource;
+describe("/cinema", () => {
+  let connection: DataSource;
 
-//   beforeAll(async () => {
-//     await AppDataSource.initialize()
-//       .then((res) => {
-//         connection = res;
-//       })
-//       .catch((err) => {
-//         console.error("Error during Data Source initialization", err);
-//       });
+  beforeAll(async () => {
+    await AppDataSource.initialize()
+      .then((res) => {
+        connection = res;
+      })
+      .catch((err) => {
+        console.error("Error during Data Source initialization", err);
+      });
 
-//     // REQUEST USER CREATION
-//     await request(app).post("/users").send(mockedAdmin);
-//     await request(app).post("/users").send(mockedUser);
-//   });
+    // REQUEST USER CREATION
+    await request(app).post("/users").send(mockedAdmin);
+    await request(app).post("/users").send(mockedUser);
+  });
 
-//   afterAll(async () => {
-//     await connection.destroy();
-//   });
+  afterAll(async () => {
+    await connection.destroy();
+  });
 
-//   test("POST /cinema -  should be able to create a cinema", async () => {
-//     const adminLoginResponse = await request(app)
-//       .post("/login")
-//       .send(mockedAdminLogin);
+  test("POST /cinema -  should be able to create a cinema", async () => {
+    const adminLoginResponse = await request(app)
+      .post("/login")
+      .send(mockedAdminLogin);
 
-//     const cinemaCreateResponse = await request(app)
-//       .post("/cinema")
-//       .set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
-//       .send(mockedCinema);
+    const cinemaCreateResponse = await request(app)
+      .post("/cinema")
+      .set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
+      .send(mockedCinema);
 
-//     expect(cinemaCreateResponse.body).toHaveProperty("name");
-//     expect(cinemaCreateResponse.body).toHaveProperty("id");
-//     expect(cinemaCreateResponse.status).toBe(201);
-//   });
+    expect(cinemaCreateResponse.body).toHaveProperty("name");
+    expect(cinemaCreateResponse.body).toHaveProperty("id");
+    expect(cinemaCreateResponse.status).toBe(201);
+  });
 
-//   test("POST /cinema -  not should be able to create a cinema already exist", async () => {
-//     const adminLoginResponse = await request(app)
-//       .post("/login")
-//       .send(mockedAdminLogin);
+  test("POST /cinema -  not should be able to create a cinema already exist", async () => {
+    const adminLoginResponse = await request(app)
+      .post("/login")
+      .send(mockedAdminLogin);
 
-//     const cinemaCreateResponse = await request(app)
-//       .post("/cinema")
-//       .set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
-//       .send(mockedCinemaExistent);
+    const cinemaCreateResponse = await request(app)
+      .post("/cinema")
+      .set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
+      .send(mockedCinemaExistent);
 
-//     expect(cinemaCreateResponse.body).toHaveProperty("message");
-//     expect(cinemaCreateResponse.status).toBe(400);
-//   });
+    expect(cinemaCreateResponse.body).toHaveProperty("message");
+    expect(cinemaCreateResponse.status).toBe(400);
+  });
 
-//   test("POST /cinema -  must not be able to create a cinema without authorization", async () => {
-//     const adminUserResponse = await request(app)
-//       .post("/login")
-//       .send(mockedUserLogin);
+  test("POST /cinema -  must not be able to create a cinema without authorization", async () => {
+    const adminUserResponse = await request(app)
+      .post("/login")
+      .send(mockedUserLogin);
 
-//     const cinemaCreateResponse = await request(app)
-//       .post("/cinema")
-//       .set("Authorization", `Bearer ${adminUserResponse.body.token}`)
-//       .send(mockedCinemaExistent);
+    const cinemaCreateResponse = await request(app)
+      .post("/cinema")
+      .set("Authorization", `Bearer ${adminUserResponse.body.token}`)
+      .send(mockedCinemaExistent);
 
-//     expect(cinemaCreateResponse.body).toHaveProperty("message");
-//     expect(cinemaCreateResponse.status).toBe(401);
-//   });
+    expect(cinemaCreateResponse.body).toHaveProperty("message");
+    expect(cinemaCreateResponse.status).toBe(401);
+  });
 
-//   test("PATCH /cinema -  should be able to update a cinema", async () => {
-//     const adminLoginResponse = await request(app)
-//       .post("/login")
-//       .send(mockedAdminLogin);
+  test("PATCH /cinema -  should be able to update a cinema", async () => {
+    const adminLoginResponse = await request(app)
+      .post("/login")
+      .send(mockedAdminLogin);
 
-//     const cinemaCreateResponse = await request(app)
-//       .patch(`/cinema/${1}`)
-//       .set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
-//       .send(mockedCinemaUpdate);
+    const cinemaCreateResponse = await request(app)
+      .patch(`/cinema/${1}`)
+      .set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
+      .send(mockedCinemaUpdate);
 
-//     expect(cinemaCreateResponse.body).toHaveProperty("name");
-//     expect(cinemaCreateResponse.body).toHaveProperty("id");
-//     expect(cinemaCreateResponse.status).toBe(201);
-//   });
+    expect(cinemaCreateResponse.body).toHaveProperty("name");
+    expect(cinemaCreateResponse.body).toHaveProperty("id");
+    expect(cinemaCreateResponse.status).toBe(201);
+  });
 
-//   test("PATCH /cinema -  should be able to update a cinema", async () => {
-//     const userLoginResponse = await request(app)
-//       .post("/login")
-//       .send(mockedUserLogin);
+  test("PATCH /cinema -  should be able to update a cinema", async () => {
+    const userLoginResponse = await request(app)
+      .post("/login")
+      .send(mockedUserLogin);
 
-//     const cinemaCreateResponse = await request(app)
-//       .patch(`/cinema/${1}`)
-//       .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
-//       .send(mockedCinemaUpdate);
+    const cinemaCreateResponse = await request(app)
+      .patch(`/cinema/${1}`)
+      .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+      .send(mockedCinemaUpdate);
 
-//     expect(cinemaCreateResponse.body).toHaveProperty("message");
-//     expect(cinemaCreateResponse.status).toBe(401);
-//   });
-// });
+    expect(cinemaCreateResponse.body).toHaveProperty("message");
+    expect(cinemaCreateResponse.status).toBe(401);
+  });
+});
