42284c3d9eded0a5b05b228ac0594b494244544d
Larissa Sato <larissasato_@outlook.com>
2022-11-09 12:25:56 -0300

    test: refactor user tests

--- src/middlewares/ensureIsAdm.middleware.ts
+++ src/middlewares/ensureIsAdm.middleware.ts
@@ -6,7 +6,7 @@ const ensureIsAdmMiddleware = async (
   next: NextFunction
 ) => {
   if (!req.user.isAdm) {
-    return res.status(401).json({
+    return res.status(403).json({
       message: "User is not adm",
     });
   }
